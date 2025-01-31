1317ed65c59c35ba517a4a95e5ef02e4bd2f1d48
João Jacques <jjacques.amann@gmail.com>
2025-01-31 00:56:33 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    return {
+        name: "skills",
+        description: "Skills",
+        options: [
+            {
+                name: "member",
+                description: "Member",
+                type: 6,
+                required: true,
+            },
+        ],
+    };
+}
